e387284d72be6742d3d52be37fe6f9d93526fbdd
ViktorMelander <Viktor.Melander@medieinstitutet.se>
2026-04-20 16:18:07 +0200

    fix centering error 2

--- src/main.js
+++ src/main.js
@@ -1,18 +1,5 @@
 import { salaryAfterTax } from './salary.js'
 
-document.querySelector('#app').innerHTML = `
-  <div style="font-family: sans-serif; padding: 20px;">
-    <h1>Lön efter skatt 💰</h1>
-
-    <input id="salary" type="number" placeholder="Bruttolön" />
-    <input id="tax" type="number" placeholder="Skatt (t.ex 0.30)" />
-
-    <button id="calc">Räkna</button>
-
-    <h2 id="result"></h2>
-  </div>
-`
-
 document.querySelector('#calc').addEventListener('click', () => {
   const salary = Number(document.querySelector('#salary').value)
   const tax = Number(document.querySelector('#tax').value)
