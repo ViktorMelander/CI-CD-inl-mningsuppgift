84e61353e7d32d9bcdcb15931874c690dc0538f1
ViktorMelander <Viktor.Melander@medieinstitutet.se>
2026-04-20 17:31:59 +0200

    fix centering 3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { salaryAfterTax } from './salary.js'
 
+document.querySelector('#app').innerHTML = `
+  <h1>Lön efter skatt 💰</h1>
+
+  <input id="salary" type="number" placeholder="Bruttolön" />
+  <input id="tax" type="number" placeholder="Skatt (t.ex 0.30)" />
+
+  <br /><br />
+
+  <button id="calc">Räkna</button>
+
+  <h2 id="result"></h2>
+`
+
 document.querySelector('#calc').addEventListener('click', () => {
   const salary = Number(document.querySelector('#salary').value)
   const tax = Number(document.querySelector('#tax').value)
